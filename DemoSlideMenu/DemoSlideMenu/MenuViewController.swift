c818c1824b0d9bdb2c48daa9da86feed0100445a
HHuyy <39610462+HHuyy@users.noreply.github.com>
2019-01-23 16:44:10 +0700

    Add files via upload

---
+++ DemoSlideMenu/DemoSlideMenu/MenuViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MenuViewController.swift
+//  DemoSlideMenu
+//
+//  Created by gia huy on 1/22/19.
+//  Copyright © 2019 gia huy. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func menuButton(_ sender: Any) {
+        NotificationCenter.default.post(name: Notification.Name("raVao"), object: nil)
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
